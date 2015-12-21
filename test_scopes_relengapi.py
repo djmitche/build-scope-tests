315ece53e5dbbbfc8a9748f539be3b2ecd05e3c4
Dustin J. Mitchell <dustin@mozilla.com>
2015-12-21 22:24:50 +0000

    add tree levels

--- test_scopes_relengapi.py
+++ test_scopes_relengapi.py
@@ -11,155 +11,45 @@ secondary layer of security.
 
 from common import assertPrincipalsWithScope
 from common import assertScopesWithPrefix
-from common import releng_permacreds
-from common import taskcluster_permacreds
+from common import expandRole
 
-def test_relengapi_tooltool_download_public():
-    """Some repos have download the ability to download public artifacts from
-    tooltool.  Since this operation can occur without credntials, too, it need
-    not be highly restricted."""
-    assertPrincipalsWithScope("docker-worker:relengapi-proxy:tooltool.download.public", [
-        # root
-        'client-id:root',
 
-        # CI testing
-        'client-id-alias:worker-ci-tests', # docker-worker integration tests
+def test_relengapi_tooltool_download():
+    """Docker-worker allows tooltool download permissions, for public or internal files, to repositories
+    at all SCM levels including SCM level 1 (try).  This is necessary to build Firefox for Android, which
+    requires non-public SDK and NDK bits."""
+    print expandRole('mozilla-group:scm_level_1'), 'moz-tree:level:1',
+    for lvl in 'public', 'internal':
+        assertPrincipalsWithScope("docker-worker:relengapi-proxy:tooltool.download." + lvl, [
+            # trees
+            expandRole('moz-tree:level:1'),
+            expandRole('moz-tree:level:2'),
+            expandRole('moz-tree:level:3'),
 
-        # services
-        'client-id-alias:funsize-dev',
-        'client-id-alias:funsize-scheduler',
-        'client-id-alias:release-runner-dev',
-        'client-id-alias:scheduler-taskcluster-net', # Bug 1218541
+            # permacreds used to download builds on bitbar
+            'client-id-alias:testdroid-worker',
 
-        # trees
-        'moz-tree:level:1',
-        'moz-tree:level:2',
-        'moz-tree:level:3',
-        'mozilla-group:scm_level_1',
-        'mozilla-group:scm_level_2',
-        'mozilla-group:scm_level_3',
+            # user groups that list the permission explicitly
+            expandRole('mozilla-group:releng'),
 
-        # repos
-        'repo:hg.mozilla.org/integration/b2g-inbound:*',
-        'repo:hg.mozilla.org/integration/fx-team:*',
-        'repo:hg.mozilla.org/integration/mozilla-inbound:*',
-        'repo:hg.mozilla.org/mozilla-central:*',
-        'repo:hg.mozilla.org/projects/alder:*',
-        'repo:hg.mozilla.org/projects/cedar:*',
-        'repo:hg.mozilla.org/projects/cypress:*',
-        'repo:hg.mozilla.org/projects/pine:*',
-        'repo:hg.mozilla.org/releases/b2g-ota:*',
-        'repo:hg.mozilla.org/releases/mozilla-b2g34_v2_1s:*',
-        'repo:hg.mozilla.org/releases/mozilla-b2g44_v2_5:*',
-        'repo:hg.mozilla.org/try:*',
+            # services
+            'client-id:aws-provisioner',
+            'client-id-alias:funsize-dev',
+            'client-id-alias:funsize-scheduler',
+            'client-id-alias:release-runner-dev',
+            'client-id-alias:scheduler-taskcluster-net',  # Bug 1218541
 
-        # people
-        releng_permacreds,
-        taskcluster_permacreds,
-        'client-id:dustin-docker-dev',
-        'client-id-alias:permacred-armenzg',
-        'client-id-alias:permacred-armenzg-testing',
-        'client-id-alias:permacred-nhirata',
-        'client-id-alias:permacred-ted',
-        'client-id-alias:temporary-credentials',
-        'client-id-alias:testdroid-worker',
-        'client-id-alias:npark',
-        'client-id-alias:sousmangoosta',
-        'client-id-alias:brson',
-        'client-id-alias:gerard-majax',
-        'client-id-alias:rwood',
-        'client-id-alias:nullaus',
-        'client-id-alias:kgrandon',
-        'client-id-alias:drs',
-        'client-id-alias:shako',
-        'client-id-alias:mihneadb',
-        'client-id-alias:russn',
-        'client-id:gandalf',
+            # worker types
+            'worker-type:aws-provisioner-v1/*',  # Bug 1233555
 
-        # user groups
-        'mozilla-group:releng',
-        'mozilla-group:team_relops',
-        'mozilla-group:team_taskcluster',
+            # root
+            'client-id:root',
 
-        # services
-        'client-id:aws-provisioner',
+            # CI testing
+            'client-id:dustin-docker-dev',
+            'client-id-alias:worker-ci-tests',  # docker-worker integration tests
+        ], omitTrusted=True)
 
-        # worker types
-        'worker-type:aws-provisioner-v1/*', # Bug 1233555
-    ], omitTrusted=True)
-
-def test_relengapi_tooltool_download_internal():
-    """Some repos have download the ability to download internal artifacts
-    from tooltool.  This extends to try-level repositories."""
-    assertPrincipalsWithScope("docker-worker:relengapi-proxy:tooltool.download.internal", [
-        # root
-        'client-id:root',
-
-        # CI testing
-        'client-id-alias:worker-ci-tests', # docker-worker integration tests
-
-        # services
-        'client-id-alias:funsize-dev',
-        'client-id-alias:funsize-scheduler',
-        'client-id-alias:release-runner-dev',
-        'client-id-alias:scheduler-taskcluster-net', # Bug 1218541
-
-        # trees
-        'moz-tree:level:1',
-        'moz-tree:level:2',
-        'moz-tree:level:3',
-        'mozilla-group:scm_level_1',
-        'mozilla-group:scm_level_2',
-        'mozilla-group:scm_level_3',
-
-        # repos
-        'repo:hg.mozilla.org/integration/b2g-inbound:*',
-        'repo:hg.mozilla.org/integration/fx-team:*',
-        'repo:hg.mozilla.org/integration/mozilla-inbound:*',
-        'repo:hg.mozilla.org/mozilla-central:*',
-        'repo:hg.mozilla.org/projects/alder:*',
-        'repo:hg.mozilla.org/projects/cedar:*',
-        'repo:hg.mozilla.org/projects/cypress:*',
-        'repo:hg.mozilla.org/projects/pine:*',
-        'repo:hg.mozilla.org/releases/b2g-ota:*',
-        'repo:hg.mozilla.org/releases/mozilla-b2g34_v2_1s:*',
-        'repo:hg.mozilla.org/releases/mozilla-b2g44_v2_5:*',
-        'repo:hg.mozilla.org/try:*',
-
-        # people
-        releng_permacreds,
-        taskcluster_permacreds,
-        'client-id:dustin-docker-dev',
-        'client-id-alias:permacred-armenzg',
-        'client-id-alias:permacred-armenzg-testing',
-        'client-id-alias:permacred-nhirata',
-        'client-id-alias:permacred-ted',
-        'client-id-alias:temporary-credentials',
-        'client-id-alias:testdroid-worker',
-        'client-id-alias:npark',
-        'client-id-alias:sousmangoosta',
-        'client-id-alias:brson',
-        'client-id-alias:gerard-majax',
-        'client-id-alias:rwood',
-        'client-id-alias:nullaus',
-        'client-id-alias:kgrandon',
-        'client-id-alias:drs',
-        'client-id-alias:shako',
-        'client-id-alias:mihneadb',
-        'client-id-alias:russn',
-        'client-id:gandalf',
-
-        # user groups
-        'mozilla-group:releng',
-        'mozilla-group:team_relops',
-        'mozilla-group:team_taskcluster',
-
-        # services
-        'client-id:aws-provisioner',
-
-        # worker types
-        'worker-type:aws-provisioner-v1/*', # Bug 1233555
-    ], omitTrusted=True)
 
 def test_relengapi_other_perms():
     """Only the relengapi permissions listed here appear anywhere in the roles"""
@@ -168,4 +58,4 @@ def test_relengapi_other_perms():
         'docker-worker:relengapi-proxy:tooltool.download.public',
         'docker-worker:*',
         '*',  # trivial match
-    ]);
+    ])
---
+++ test_tree_levels.py
@@ -0,0 +1,60 @@
+"""
+Tests for access to the moz-tree:level:* roles
+"""
+
+from common import assertPrincipalsWithRole
+from common import expandRole
+
+l1_repos = [
+    'repo:hg.mozilla.org/try:*',
+]
+
+l2_repos = [
+    'repo:hg.mozilla.org/projects/alder:*',
+    'repo:hg.mozilla.org/projects/cedar:*',
+    'repo:hg.mozilla.org/projects/cypress:*',
+    'repo:hg.mozilla.org/projects/pine:*',
+]
+
+l3_repos = [
+    'repo:hg.mozilla.org/integration/b2g-inbound:*',
+    'repo:hg.mozilla.org/integration/fx-team:*',
+    'repo:hg.mozilla.org/integration/mozilla-inbound:*',
+    'repo:hg.mozilla.org/mozilla-central:*',
+    'repo:hg.mozilla.org/releases/b2g-ota:*',
+    'repo:hg.mozilla.org/releases/mozilla-b2g34_v2_1s:*',
+    'repo:hg.mozilla.org/releases/mozilla-b2g44_v2_5:*',
+]
+
+def test_tree_level_3():
+    assertPrincipalsWithRole('moz-tree:level:3', [
+        # level-3 people and repos
+        expandRole('mozilla-group:scm_level_3'), l3_repos,
+
+        # CI testing
+        'client-id:dustin-docker-dev',
+        'client-id-alias:worker-ci-tests',  # docker-worker integration tests
+
+        # permacreds used to download builds on bitbar
+        'client-id-alias:testdroid-worker',  # Bug 1218549
+
+        # services
+        'client-id:aws-provisioner',
+
+        # worker types
+        'worker-type:aws-provisioner-v1/*',  # Bug 1233555
+    ], omitTrusted=True)
+
+def test_tree_level_2():
+    assertPrincipalsWithRole('moz-tree:level:2', [
+        # level 3, plus level-2 people and repos
+        expandRole('mozilla-group:scm_level_2'), l2_repos,
+        expandRole('moz-tree:level:3'),
+    ], omitTrusted=True)
+
+def test_tree_level_1():
+    assertPrincipalsWithRole('moz-tree:level:1', [
+        # level 2, plus level-2 people and repos
+        expandRole('mozilla-group:scm_level_1'), l1_repos,
+        expandRole('moz-tree:level:2'),
+    ], omitTrusted=True)
